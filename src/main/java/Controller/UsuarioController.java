dda768408468182f6baeb1c548161dcc3476ebbc
Sebas <sebastian.roidzaid@gmail.com>
2020-08-20 18:16:45 -0300

    Conexion con BD

--- src/main/java/Controller/UsuarioController.java
+++ src/main/java/Controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST,RequestMethod.DELETE})
 @RequestMapping("usuarios")
 public class UsuarioController {
     @Autowired
