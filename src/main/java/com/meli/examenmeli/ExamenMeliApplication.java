37f278bb61e3e22445e3901e5aa95b80eb407949
Sebas <sebastian.roidzaid@gmail.com>
2020-08-20 02:20:28 -0300

    primer commit

---
+++ src/main/java/com/meli/examenmeli/ExamenMeliApplication.java
@@ -0,0 +1,13 @@
+package com.meli.examenmeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMeliApplication.class, args);
+	}
+
+}
