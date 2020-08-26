7b84ad7baa9057c012128fe35d9cc21c0f191a09
Sebas <sebastian.roidzaid@gmail.com>
2020-08-26 14:38:15 -0300

    Corrección calculo de Horarios

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,5 +1,5 @@
 
 DROP TABLE IF EXISTS log;
 CREATE TABLE log(id serial PRIMARY KEY, country VARCHAR(255), distance NUMERIC(10,2), invocations NUMERIC);
-INSERT INTO log(country, distance, invocations) VALUES('Argentina', 1500, 20);
-INSERT INTO log(country, distance, invocations) VALUES('Brasil', 5000, 5);
+--INSERT INTO log(country, distance, invocations) VALUES('Argentina', 1500, 20);
+--INSERT INTO log(country, distance, invocations) VALUES('Brasil', 5000, 5);
